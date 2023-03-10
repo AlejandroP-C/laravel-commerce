1c34e8d80a02971177284a024aeae074cbed52f7
IanGarcia <ianGarcia@gmail.com>
2023-03-10 09:45:37 +0100

    Commir arreglo Adminphp

--- routes/admin.php
+++ routes/admin.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\CommerceController;
-
+use App\Http\Controllers\Admin\ProductController;
 
 Route::get('', [HomeController::class, 'index'])->name('admin.home');
 Route::resource('categories', CategoryController::class)->names('admin.categories');
